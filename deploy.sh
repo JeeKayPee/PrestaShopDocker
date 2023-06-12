191ae2b4fdb652d9b39844359826b94a02cd6c22
Gaurav Pandey <45666264+JeeKayPee@users.noreply.github.com>
2023-06-12 11:46:12 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install docker.io --yes
+# create a network for containers to communicate
+sudo docker network create prestashop-net
+# launch mysql 5.7 container
+sudo docker run -ti --name some-mysql --network prestashop-net -e MYSQL_ROOT_PASSWORD=admin -p 3307:3306 -d mysql:5.7
+# launch prestashop container
+sudo docker run -ti --name some-prestashop --network prestashop-net -e DB_SERVER=some-mysql -p 8080:80 -d prestashop/prestashop:1.6
